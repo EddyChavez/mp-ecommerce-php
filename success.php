420cca30fce1d9d266819890de0e0f19e778e0b6
EDGALON <14240683@itleon.edu.mx>
2020-08-30 21:08:45 -0500

    Terminado

---
+++ success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprovado</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css%22%3E
+</head>
+<body>
+    <h1 class="text-center">Success</h1>
+    <h2>
+        <?php
+            $v1 = $_GET['collection_id']; 
+            echo $v1;
+        ?>
+    </h2>
+</body>
+</html>
